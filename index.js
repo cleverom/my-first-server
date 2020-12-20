7b8d42f3ee9a97c2bb143e96e1cac0d10257044a
cleverom <clevereziogor@gmail.com>
2020-12-20 18:18:32 +0100

    create filepath, css and html pages

--- index.js
+++ index.js
@@ -11,7 +11,77 @@ const server = http.createServer((req, res) => {
         })
         
     }
-})
-const PORT = process.env.PORT || 5000
+    // if(req.url === '/about'){
+    //     fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
+    //         if(err) throw err;
+    //         res.writeHead(200, { 'content-Type': 'text/html'})
+    //         res.end(content)
+    //     })
+        
+    // }
+    // if(req.url === '/api/users'){
+    //     const users = [
+    //         { name: "clever", age: 40 },
+    //         { name: "hilton", age: 50 }
+    // ];
+    // res.writeHead(200, { 'Content-Type': 'application/json' });
+    // res.end(JSON.stringify(users))
+    // }
+
+    // Build file path 
+    let filepath = path.join(__dirname, 'public', req.url === '/' ? 'index.html': req.url);
+    
+
+    // extension of file
+
+    let extname = path.extname(filepath);
+
+    // initial content type
+    let contentType = 'text/html'
+
+    // check ext and set conyent type
+    switch(extname) {
+        case'.js':
+            contentType = 'text/javascript';
+            break;
+        case'.css':
+            contentType = 'text/css';
+            break;
+        case'.json':
+            contentType = 'application/json';
+            break;
+        case'.png':
+            contentType = 'image/jpg';
+            break;
+        case'.jpg':
+            contentType = 'image/jpg';
+            break;
+    }
+
+    //readFile
+
+    fs.readFile(filepath, (err, content) => {
+        if(err) {
+            if(err.code === 'ENOENT'){
+                //page not Found
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) =>{
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf8');
+                })
+            } else{
+                //some server error
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        }else{
+            //success
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf8');
+        }
+    }); 
+
+});
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log(`server running on port ${PORT}`));
+
